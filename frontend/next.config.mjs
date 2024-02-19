5d34e13daa4cf8c49574be7cc982afe184139972
larry <larrylongdev@gmail.com>
2024-02-19 17:23:47 -0500

    update config

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -4,9 +4,6 @@ const nextConfig = {
         SERVER_PROTOCOL: "http",
         SERVER_HOST: "localhost",
         SERVER_PORT: "3001",
-        CLIENT_PROTOCOL: process.env.CLIENT_PROTOCOL,
-        CLIENT_HOST: process.env.CLIENT_HOST,
-        CLIENT_PORT: process.env.CLIENT_PORT,
       },
 };
 
