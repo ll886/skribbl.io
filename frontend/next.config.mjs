295f0eb142c2b605c5c1d2a63702890906c23e06
larry <larrylongdev@gmail.com>
2024-02-19 17:22:18 -0500

    socket setup

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    env: {
+        SERVER_PROTOCOL: "http",
+        SERVER_HOST: "localhost",
+        SERVER_PORT: "3001",
+        CLIENT_PROTOCOL: process.env.CLIENT_PROTOCOL,
+        CLIENT_HOST: process.env.CLIENT_HOST,
+        CLIENT_PORT: process.env.CLIENT_PORT,
+      },
+};
 
 export default nextConfig;
