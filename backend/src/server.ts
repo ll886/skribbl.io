abdb29e2ca5c1ef88efdda4659afbd4562929b2d
Larry <59991196+ll886@users.noreply.github.com>
2024-03-02 00:09:43 -0500

    Create and join rooms (#5)
    * socket setup
    
    * update config
    
    * create room routes
    
    * refactor
    
    * add join and leave room functionality
    
    * refactor

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,8 @@
 import express from "express";
+import cors from "cors";
 import { createServer } from 'node:http';
 import { initSocket } from './socket.js';
+import roomsRouter from './routes.rooms.js';
 
 // init app
 const port = 3001
@@ -10,8 +12,14 @@ const app = express();
 const server = createServer(app);
 initSocket(server);
 app.use(express.json());
+app.use(cors({
+  origin: [
+    "http://localhost:3000",
+  ],
+}))
 
 // app routes
+app.use("/api/rooms", roomsRouter);
 
 server.listen(port, () => {
   console.log(`${protocol}://${host}:${port}`);
