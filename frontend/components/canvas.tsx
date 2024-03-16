53f5ce9f4dec46cfae8167dfc964797af7cf36ae
Kaung Zan <khz24@drexel.edu>
2024-03-16 00:58:16 -0400

    socket draw

--- frontend/components/canvas.tsx
+++ frontend/components/canvas.tsx
@@ -1,75 +1,85 @@
-"use client";
+'use client'
 
-import { useDraw } from "@/hooks/useDraw";
-import { useState } from "react";
-import { CompactPicker } from "react-color";
+import { FC, useEffect, useState } from 'react'
+import { useDraw } from '../hooks/useDraw'
+import { ChromePicker } from 'react-color'
 
-export default function Canvas() {
-  const [color, setColor] = useState<string>('#000');
-  const [width, setWidth] = useState<number>(5);
-  const { canvasRef, onMouseDown, clear } = useDraw(drawLine);
+import { io } from 'socket.io-client'
+import { drawLine } from '../util/drawLine'
+const socket = io('http://localhost:3001')
 
-  function drawLine({ prevPoint, currentPoint, context }: Draw) {
-    const { x: currX, y: currY } = currentPoint;
-    const lineColor = color;
-    const lineWidth = width;
+interface pageProps {}
 
-    let startPoint = prevPoint ?? currentPoint;
-    context.beginPath();
-    context.lineWidth = lineWidth;
-    context.strokeStyle = lineColor;
-    context.moveTo(startPoint.x, startPoint.y);
-    context.lineTo(currX, currY);
-    context.stroke();
+type DrawLineProps = {
+  prevPoint: Point | null
+  currentPoint: Point
+  color: string
+}
 
-    context.fillStyle = lineColor;
-    context.beginPath();
-    context.arc(startPoint.x, startPoint.y, 2, 0, 2 * Math.PI);
-    context.fill();
+const page: FC<pageProps> = () => {
+  const [color, setColor] = useState<string>('#000')
+  const { canvasRef, onMouseDown, clear } = useDraw(createLine)
+
+  useEffect(() => {
+    const ctx = canvasRef.current?.getContext('2d')
+
+    socket.emit('client-ready')
+
+    socket.on('get-canvas-state', () => {
+      if (!canvasRef.current?.toDataURL()) return
+      console.log('sending canvas state')
+      socket.emit('canvas-state', canvasRef.current.toDataURL())
+    })
+
+    socket.on('canvas-state-from-server', (state: string) => {
+      console.log('I received the state')
+      const img = new Image()
+      img.src = state
+      img.onload = () => {
+        ctx?.drawImage(img, 0, 0)
+      }
+    })
+
+    socket.on('draw-line', ({ prevPoint, currentPoint, color }: DrawLineProps) => {
+      if (!ctx) return console.log('no ctx here')
+      drawLine({ prevPoint, currentPoint, ctx, color })
+    })
+
+    socket.on('clear', clear)
+
+    return () => {
+      socket.off('draw-line')
+      socket.off('get-canvas-state')
+      socket.off('canvas-state-from-server')
+      socket.off('clear')
+    }
+  }, [canvasRef])
+
+  function createLine({ prevPoint, currentPoint, ctx }: Draw) {
+    socket.emit('draw-line', { prevPoint, currentPoint, color })
+    drawLine({ prevPoint, currentPoint, ctx, color })
   }
 
   return (
-    <>
-      <div className="w-full flex justify-center items-center mb-4">
-        <canvas
-          ref={canvasRef}
-          onMouseDown={onMouseDown}
-          width={750}
-          height={750}
-          className="border border-black rounded-md"
-        />
+    <div className='w-full flex justify-center items-center mb-4'>
+      <div className='flex flex-col gap-10 pr-10'>
+        <ChromePicker color={color} onChange={(e) => setColor(e.hex)} />
+        <button
+          type='button'
+          className='p-2 rounded-md border border-black'
+          onClick={() => socket.emit('clear')}>
+          Clear canvas
+        </button>
       </div>
-      <div className="w-full flex justify-center items-center">
-        <div>
-          <CompactPicker 
-            color={color} 
-            onChange={(e) => {
-              setColor(e.hex)
-              setWidth(5)
-            }}
-          />
-        </div>
-        <div>
-          <button
-            type="button"
-            className="p-2 rounded-md border border-black"
-            onClick={() => {
-              setColor('#FFF')
-              setWidth(20)
-            }}
-          >
-            Eraser
-          </button>
-          <br></br>
-          <button
-            type="button"
-            className="p-2 rounded-md border border-black"
-            onClick={clear}
-          >
-            Clear canvas
-          </button>
-        </div>
-      </div>
-    </>
-  );
+      <canvas
+        ref={canvasRef}
+        onMouseDown={onMouseDown}
+        width={750}
+        height={750}
+        className='border border-black rounded-md'
+      />
+    </div>
+  )
 }
+
+export default page
