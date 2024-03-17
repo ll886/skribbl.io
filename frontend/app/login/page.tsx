141b56db7b8f1190bb9b137c29a19ff6a600837d
larry <larrylongdev@gmail.com>
2024-03-17 03:37:14 -0400

    add trailling slash to urls

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -32,7 +32,7 @@ export default function Login() {
         }
 
         await fetch(
-            `${serverUrl}/api/login`,
+            `${serverUrl}/api/login/`,
             {
                 method: 'POST',
                 headers: {
